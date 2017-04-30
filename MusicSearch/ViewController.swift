ad2f9b61039747e4b87d0d0da06326ce3bb5eb2f
植田正裕 <ueda@masahiro-no-imac.local>
2017-04-30 22:51:55 +0900

    first commit

--- MusicSearch/ViewController.swift
+++ MusicSearch/ViewController.swift
@@ -13,13 +13,34 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.showSongs()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    private func showSongs() {
+        let urlString = "https://itunes.apple.com/search?term=Beatles&entity=musicTrack&limit=5&lang=ja_jp&country=JP"
+        
+        
+        let url = URL(string: urlString)
+        let task = URLSession.shared.dataTask(with: url!){ data, response, error in
+            if let data = data, let response = response {
+                do{
+                    let json = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments)
+                    print(json)
+                }catch{
+                    print("Json Serialize Error")
+                }
+            }else{
+                print(error ?? "Error")
+            }
+        }.resume()
+   
+    }
 
 }
 
