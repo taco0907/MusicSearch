6841bf5aa5b7a462693400c7212f6386fc0fbe7d
taco0907 <taco0907@gmail.com>
2017-05-07 23:29:25 +0900

    fix TableView

--- MusicSearch/ViewController.swift
+++ MusicSearch/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 var myTableView: UITableView!
-var items: [String] = []
+var items_title: [String] = []
+var items_image: [UIImage] = []
 
 class ViewController: UIViewController ,UITableViewDataSource, UITableViewDelegate{
 
@@ -20,12 +21,14 @@ class ViewController: UIViewController ,UITableViewDataSource, UITableViewDelega
         
         let displayWidth:CGFloat = view.self.frame.width
         let displayHeight:CGFloat = view.self.frame.height
-        
-        myTableView = UITableView(frame: CGRect(x: 10, y: 10, width: displayWidth, height: displayHeight))
-        myTableView.dataSource = self
-        self.view.addSubview(myTableView)
-        
+        let barHeight:CGFloat = UIApplication.shared.statusBarFrame.size.height
         self.showSongs()
+        
+        
+        myTableView = UITableView(frame: CGRect(x: 0, y: barHeight, width: displayWidth, height: displayHeight))
+        myTableView.dataSource = self
+        
+        self.view.addSubview(myTableView)
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,20 +38,36 @@ class ViewController: UIViewController ,UITableViewDataSource, UITableViewDelega
     
     
     private func showSongs() {
-        let urlString = "https://itunes.apple.com/search?term=Beatles&entity=musicTrack&limit=5&lang=ja_jp&country=JP"
-        
+        let urlString = "https://itunes.apple.com/search?term=bjork&entity=album&limit=15&lang=ja_jp&country=JP"
         
         let url = URL(string: urlString)
-        let task = URLSession.shared.dataTask(with: url!){ data, response, error in
+        let task = URLSession.shared.dataTask(with: url! as URL){ data, response, error in
             if let data = data, let response = response {
                 do{
                     let json = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments) as! [String:Any]
                     let jsondata = json["results"] as! [[String:Any]]
                     
                     for song in jsondata {
-                        let trackName = song["trackName"] as! String
-                        items.append(trackName)
+                        let albumName = song["collectionCensoredName"] as! String
+                        items_title.append(albumName)
                     }
+                    DispatchQueue.main.async {
+                        myTableView.reloadData()
+                    }
+                    myTableView.reloadData()
+                    
+                    for song in jsondata{
+                        let imageUrl = song["artworkUrl60"] as! String
+                        let url = URL(string: imageUrl)
+                        let imageData = NSData(contentsOf: url!)
+                        let image = UIImage(data: imageData! as Data)
+                        items_image.append(image!)
+                    }
+                    DispatchQueue.main.async {
+                        myTableView.reloadData()
+                    }
+                    
+                    
                 }catch{
                     print("Json Serialize Error")
                 }
@@ -56,17 +75,25 @@ class ViewController: UIViewController ,UITableViewDataSource, UITableViewDelega
                 print(error ?? "Error")
             }
         }.resume()
-   
+        
     }
     
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        return items_title.count
     }
   
     
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.value1, reuseIdentifier: "MyCell")
-        cell.textLabel?.text = items[indexPath.row]
+        
+        
+        if(indexPath.row < items_title.count) {
+            cell.textLabel?.text = items_title[indexPath.row]
+        }
+        
+        if(indexPath.row < items_image.count){
+            cell.imageView?.image = items_image[indexPath.row]
+        }
         
         return cell   
     }
